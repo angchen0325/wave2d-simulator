2ecccb6ba2c88bec639b376d7558e4dc8a6be94c
Ang Chen <chenang@outlook.com>
2025-04-13 22:27:47 +0800

    Update some files in the src folder

--- src/scene_objects/source.py
+++ src/scene_objects/source.py
@@ -1,13 +1,10 @@
-import sys 
-sys.path.append("../")
+from wave_sim2d.wave_simulation import SceneObject
 import cupy as cp
+import numpy as np
 import math
 
-import wave_simulation
 
-
-
-class PointSource(wave_simulation.SceneObject):
+class PointSource(SceneObject):
     """
     Implements a point source scene object. The amplitude can be optionally modulated using a modulator object.
     :param x: source position x.
@@ -18,6 +15,7 @@ class PointSource(wave_simulation.SceneObject):
     :param amp_modulator: optional amplitude modulator. This can be used to change the amplitude of the source
                           over time.
     """
+
     def __init__(self, x, y, frequency, amplitude=1.0, phase=0, amp_modulator=None):
         self.x = x
         self.y = y
@@ -29,7 +27,12 @@ class PointSource(wave_simulation.SceneObject):
     def set_amplitude_modulator(self, func):
         self.amplitude_modulator = func
 
-    def render(self, wave_speed_field, dampening_field):
+    def render(
+        self,
+        field: cp.ndarray,
+        wave_speed_field: cp.ndarray,
+        dampening_field: cp.ndarray,
+    ):
         pass
 
     def update_field(self, field, t):
@@ -41,13 +44,87 @@ class PointSource(wave_simulation.SceneObject):
         v = cp.sin(self.phase + self.frequency * t) * amplitude
         field[self.y, self.x] = v
 
+    def render_visualization(self, image: np.ndarray):
+        """renders a visualization of the scene object to the image"""
+        pass
+
+
+class LineSource(SceneObject):
+    """
+    Implements a line source scene object. The amplitude can be optionally modulated using a modulator object.
+    The source emits along a line defined by a start and end point.
+    :param start: starting (x, y) coordinates of the line as a tuple.
+    :param end: ending (x, y) coordinates of the line as a tuple.
+    :param frequency: emitting frequency.
+    :param amplitude: emitting amplitude, not used when an amplitude modulator is given
+    :param phase: emitter phase
+    :param amp_modulator: optional amplitude modulator. This can be used to change the amplitude of the source
+                          over time.
+    """
+
+    def __init__(
+        self, start, end, frequency, amplitude=1.0, phase=0, amp_modulator=None
+    ):
+        self.start = start
+        self.end = end
+        self.frequency = frequency
+        self.amplitude = amplitude
+        self.phase = phase
+        self.amplitude_modulator = amp_modulator
+
+    def set_amplitude_modulator(self, func):
+        self.amplitude_modulator = func
+
+    def render(
+        self,
+        field: cp.ndarray,
+        wave_speed_field: cp.ndarray,
+        dampening_field: cp.ndarray,
+    ):
+        pass
+
+    def update_field(self, field, t):
+        if self.amplitude_modulator is not None:
+            amplitude = self.amplitude_modulator(t) * self.amplitude
+        else:
+            amplitude = self.amplitude
+
+        v = cp.sin(self.phase + self.frequency * t) * amplitude
+
+        # Determine the points along the line using NumPy
+        x1, y1 = self.start
+        x2, y2 = self.end
+
+        distance = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        num_points = int(distance) + 1
+
+        if num_points > 0:
+            x_coords = cp.linspace(x1, x2, num_points).round().astype(int)
+            y_coords = cp.linspace(y1, y2, num_points).round().astype(int)
+
+            # Create boolean masks for valid indices
+            valid_x = (x_coords >= 0) & (x_coords < field.shape[1])
+            valid_y = (y_coords >= 0) & (y_coords < field.shape[0])
+            valid_indices = valid_x & valid_y
+
+            # Use these valid indices to update the field directly
+            valid_y_coords = y_coords[valid_indices]
+            valid_x_coords = x_coords[valid_indices]
+            field[valid_y_coords, valid_x_coords] = v
+
+    def render_visualization(self, image: np.ndarray):
+        """renders a visualization of the scene object to the image"""
+        pass
+
 
 # --- Modulators -------------------------------------------------------------------------------------------------------
+
 
 class ModulatorSmoothSquare:
     """
     A modulator that creates a smoothed square wave
     """
+
     def __init__(self, frequency, phase, smoothness=0.5):
         self.frequency = frequency
         self.phase = phase
@@ -55,7 +132,9 @@ class ModulatorSmoothSquare:
 
     def __call__(self, t):
         s = math.pow(self.smoothness, 4.0)
-        a = (0.5 / math.atan(1.0/s)) * math.atan(math.sin(t * self.frequency + self.phase) / s)+0.5
+        a = (0.5 / math.atan(1.0 / s)) * math.atan(
+            math.sin(t * self.frequency + self.phase) / s
+        ) + 0.5
         return a
 
 
@@ -63,6 +142,7 @@ class ModulatorDiscreteSignal:
     """
     A modulator that creates a smoothed binary signal
     """
+
     def __init__(self, signal_array, time_factor, transition_slope=8.0):
         self.signal_array = signal_array
         self.time_factor = time_factor
@@ -74,18 +154,20 @@ class ModulatorDiscreteSignal:
 
         # Wrap around the position if it's outside the array range
         sl = len(self.signal_array)
-        t = math.fmod(t*self.time_factor, sl)
+        t = math.fmod(t * self.time_factor, sl)
 
         # Find the indices of the neighboring values
         index_low = int(t)
         index_high = (index_low + 1) % sl
 
         # Calculate the interpolation factor
-        tf = (t - index_low)
-        tf = max(0.0, min(1.0, (tf-0.5)*self.transition_slope+0.5))
+        tf = t - index_low
+        tf = max(0.0, min(1.0, (tf - 0.5) * self.transition_slope + 0.5))
 
         # Use smooth step to interpolate between neighboring values
-        ll = smooth_step(tf) 
-        interpolated_value = (1 - ll) * self.signal_array[index_low] + ll * self.signal_array[index_high]
+        l = smooth_step(tf)
+        interpolated_value = (1 - l) * self.signal_array[
+            index_low
+        ] + l * self.signal_array[index_high]
 
         return interpolated_value
--- src/wave_simulation.py
+++ src/wave_simulation.py
@@ -1,31 +1,34 @@
-from abc import ABC, abstractmethod
-
 import cupy
+import numpy as np
 import cupy as cp
 import cupyx.scipy.signal
+from abc import ABC, abstractmethod
 
 
 class SceneObject(ABC):
     """
-    Interface for simulation scene objects.
-    A scene object is anything defining or modifying the simulation scene.
-    For example: Light sources, absorbers or regions with specific refractive index.
-    Scene objects can change the simulated field and draw their contribution to the wave speed field
-    and dampening field each frame.
-    """
+    Interface for simulation scene objects. A scene object is anything defining or modifying the simulation scene.
+    For example: Light sources, Absorbers or regions with specific refractive index. Scene objects can change the
+    simulated field and draw their contribution to the wave speed field and dampening field each frame"""
 
     @abstractmethod
-    def render(self, wave_speed_field: cupy.ndarray, dampening_field: cupy.ndarray):
-        """
-        Rendering the scene objects that contribute to the wave speed field and dampening field.
-        """
+    def render(
+        self,
+        field: cupy.ndarray,
+        wave_speed_field: cupy.ndarray,
+        dampening_field: cupy.ndarray,
+    ):
+        """renders the scene objects contribution to the wave speed field and dampening field"""
         pass
 
     @abstractmethod
     def update_field(self, field: cupy.ndarray, t):
-        """
-        Performing updates to the field itself, e.g. for adding sources.
-        """
+        """performs updates to the field itself, e.g. for adding sources"""
+        pass
+
+    @abstractmethod
+    def render_visualization(self, image: np.ndarray):
+        """renders a visualization of the scene object to the image"""
         pass
 
 
@@ -36,7 +39,7 @@ class WaveSimulator2D:
     source frequency should be adjusted accordingly
     """
 
-    def __init__(self, w, h, scene_objects):
+    def __init__(self, w, h, scene_objects, initial_field=None):
         """
         Initialize the 2D wave simulator.
         @param w: Width of the simulation grid.
@@ -49,6 +52,13 @@ class WaveSimulator2D:
         self.d = cp.ones((h, w), dtype=cp.float32)  # dampening field
         self.u = cp.zeros((h, w), dtype=cp.float32)  # field values
         self.u_prev = cp.zeros((h, w), dtype=cp.float32)  # field values of prev frame
+
+        if initial_field is not None:
+            assert w == initial_field.shape[1] and h == initial_field.shape[2], (
+                "width/height of initial field invalid"
+            )
+            self.u[:] = initial_field
+            self.u_prev[:] = initial_field
 
         # Define Laplacian kernel
         self.laplacian_kernel = cp.array(
@@ -98,7 +108,7 @@ class WaveSimulator2D:
         self.d.fill(1.0)
 
         for obj in self.scene_objects:
-            obj.render(self.c, self.d)
+            obj.render(self.u, self.c, self.d)
 
         for obj in self.scene_objects:
             obj.update_field(self.u, self.t)
@@ -109,3 +119,13 @@ class WaveSimulator2D:
         @return: A 2D array representing the simulation field.
         """
         return self.u
+
+    def render_visualization(self, image=None):
+        # clear wave speed field and dampening field
+        if image is None:
+            image = np.zeros((self.c.shape[0], self.c.shape[1], 3), dtype=np.uint8)
+
+        for obj in self.scene_objects:
+            obj.render_visualization(image)
+
+        return image
